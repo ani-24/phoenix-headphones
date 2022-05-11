83e7ecffd9cc53af4d05d1f7cd88b52aacf927b8
Aniket <rajeshwarkumar9204@gmail.com>
2022-05-11 09:08:40 +0530

    create ecommerce functionality

---
+++ components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+
+import Cart from "./Cart";
+import { useStateContext } from "../context/StateContext";
+
+const Navbar = () => {
+  const { showCart, setShowCart, totalQty } = useStateContext();
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">boAt Headphones</Link>
+      </p>
+      <button className="cart-icon" onClick={() => setShowCart(true)}>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{totalQty}</span>
+      </button>
+      {showCart && <Cart />}
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ context/StateContext.js
@@ -0,0 +1,110 @@
+import react, { createContext, useContext, useState, useEffect } from "react";
+
+import { toast } from "react-hot-toast";
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQty, setTotalQty] = useState(0);
+  const [qty, setQty] = useState(1);
+
+  let foundProduct;
+  let index;
+
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    );
+    setTotalPrice(
+      (previousTotalPrice) => previousTotalPrice + product.price * quantity
+    );
+    setTotalQty((prevTotalQty) => prevTotalQty + quantity);
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          };
+        }
+      });
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+      setCartItems([...cartItems, { ...product }]);
+    }
+    toast.success(`${qty} ${product.name} added to cart`);
+  };
+
+  const onRemove = (product) => {
+    foundProduct = cartItems.find((item) => item._id === product._id);
+    const newCartItems = cartItems.filter((item) => item._id !== product._id);
+    setTotalPrice(
+      (prevTotalPrice) =>
+        prevTotalPrice - foundProduct.price * foundProduct.quantity
+    );
+    setTotalQty((prevTotalQty) => prevTotalQty - foundProduct.quantity);
+    setCartItems(newCartItems);
+  };
+
+  const toggleCartItemQuantity = (id, value) => {
+    foundProduct = cartItems.find((item) => item._id === id);
+    index = cartItems.findIndex((product) => product._id === id);
+    const newCartItems = cartItems.filter((item) => item._id !== id);
+    if (value === "inc") {
+      newCartItems.splice(index, 0, {
+        ...foundProduct,
+        quantity: foundProduct.quantity + 1,
+      });
+      setCartItems(newCartItems);
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+      setTotalQty((prevTotalQty) => prevTotalQty + 1);
+    } else if (value === "dec") {
+      if (foundProduct.quantity > 1) {
+        newCartItems.splice(index, 0, {
+          ...foundProduct,
+          quantity: foundProduct.quantity - 1,
+        });
+        setCartItems(newCartItems);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
+        setTotalQty((prevTotalQty) => prevTotalQty - 1);
+      }
+    }
+  };
+
+  const incQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+
+  const decQty = () => {
+    setQty((prevQty) => (prevQty <= 1 ? 1 : prevQty - 1));
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        totalPrice,
+        totalQty,
+        qty,
+        showCart,
+        cartItems,
+        setShowCart,
+        setCartItems,
+        setTotalPrice,
+        setTotalQty,
+        incQty,
+        decQty,
+        onAdd,
+        onRemove,
+        toggleCartItemQuantity,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(Context);
